5d04742afad4c5d9951adabffbdaa079a99028f4
red28mil <maeliosa@live.com>
2021-04-07 10:07:25 +0100

    another install

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 
 const Hapi = require("@hapi/hapi");
 const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
+const Handlebars = require("handlebars");
+const Cookie = require("@hapi/cookie");
+const Joi = require("@hapi/joi");
+
 require("./app/models/db");
+const env = require("dotenv");
+
+const dotenv = require("dotenv");
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
 
 const server = Hapi.server({
-  port: 5020,
+  port: process.env.PORT || 5020,
   host: "localhost",
 });
 
